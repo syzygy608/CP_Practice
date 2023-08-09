3fdf1ee234b23b122ebd320de6e84a3a96b6574b
syzygy608 <hankwu99@gmail.com>
2023-08-09 21:56:36 +0800

    fix

--- cses/OneBitPositions.cpp
+++ cses/OneBitPositions.cpp
@@ -14,33 +14,34 @@ const ld PI = acos(-1);
 int N, M, K;
 vector<cd> A(SIZE), B(SIZE);
 
-void fft(vector<cd> &a, bool inv){
-    int N = (int) a.size();
-
-    for(int i = 1, j = 0; i < N; i++){
-        int bit = N>>1;
-        for(; j&bit; bit >>= 1)
+void fft(vector<cd> &a, bool inv)
+{
+    int N = (int)a.size();
+    for(int i = 1, j = 0; i < N; i++)
+    {
+        int bit = N >> 1;
+        for(; j & bit; bit >>= 1)
             j ^= bit;
         j ^= bit;
-
         if(i < j)
             swap(a[i], a[j]);
     }
-
-    for(int len = 2; len <= N; len <<= 1){
+    for(int len = 2; len <= N; len <<= 1)
+    {
         ld theta = 2*PI / len * (inv ? -1 : 1);
         cd wlen(cos(theta), sin(theta));
-        for(int i = 0; i < N; i += len){
+        for(int i = 0; i < N; i += len)
+        {
             cd w(1);
-            for(int j = 0; j < len / 2; j++){
-                cd u = a[i+j], v = a[i+j+len/2] * w;
-                a[i+j] = u + v;
-                a[i+j+len/2] = u - v;
+            for(int j = 0; j < len / 2; j++)
+            {
+                cd u = a[i + j], v = a[i + j + len / 2] * w;
+                a[i + j] = u + v;
+                a[i + j + len / 2] = u - v;
                 w *= wlen;
             }
         }
     }
-
     if(inv)
         for(cd &z : a)
             z /= N;
